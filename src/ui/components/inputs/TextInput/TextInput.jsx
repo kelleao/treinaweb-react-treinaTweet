5629af055ec0212b6331e663a7d3a5a81472612f
Raquel Leao <raquel179@gmail.com>
2022-08-10 19:28:51 -0300

    react treinatweet

---
+++ src/ui/components/inputs/TextInput/TextInput.jsx
@@ -0,0 +1,10 @@
+import styles from './TextInput.module.css';
+
+
+export default function TextInput(props) {
+    return (
+        <div>
+            <textarea className={styles['input']} {...props} />
+        </div>
+    );
+}
